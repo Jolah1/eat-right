f00793e9b627c9050209b4da686404676e93643c
okunlade Jolade <jokunlade@gmail.com>
2023-04-19 19:31:45 +0100

    food cuisine upated

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,9 +5,6 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 
-
-
-
 function Category() {
   return (
     <List>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -8,7 +8,7 @@ function Pages() {
    
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/cuisine" element={<Cuisine />}/>  
+      <Route path="/cuisine/:type" element={<Cuisine />} />  
     </Routes>
    
   );
